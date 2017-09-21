53466113ca8da85644f9d88326966e25e0ba5c6d
Valeria Aleshko <aleshko.valery@gmail.com>
2017-09-22 00:14:45 +0300

    Add UnitOfWork implementation

---
+++ DAL/Repositories/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data.Entity;
+using DAL.Interfaces.Repositories;
+
+namespace DAL.Repositories
+{
+    public class UnitOfWork: IUnitOfWork
+    {
+        private readonly DbContext _context;
+
+        public UnitOfWork(DbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            //ToAsk Is that checking must be here?
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
