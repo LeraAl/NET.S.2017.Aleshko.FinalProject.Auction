8dceea57f67da8fd18b522c0bec72c672d093778
Valeria Aleshko <aleshko.valery@gmail.com>
2017-09-22 19:14:07 +0300

    Add try-catch and logging

--- DAL/Repositories/UnitOfWork.cs
+++ DAL/Repositories/UnitOfWork.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using System.Data;
 using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Validation;
 using DAL.Interfaces.Repositories;
+using DAL.Logger;
 
 namespace DAL.Repositories
 {
     public class UnitOfWork: IUnitOfWork
     {
+        private readonly ILogger _logger = LoggerAdapter.GetLogger();
         private readonly DbContext _context;
 
         public UnitOfWork(DbContext context)
@@ -16,7 +21,42 @@ namespace DAL.Repositories
 
         public void Commit()
         {
-            _context.SaveChanges();
+            try
+            {
+                _context.SaveChanges();
+            }
+
+            catch (DbEntityValidationException ex)
+            {
+                foreach (var item in ex.EntityValidationErrors)
+                {
+                    var entry = item.Entry;
+                    var entityTypeName = entry.Entity.GetType().Name;
+                    
+                    foreach (var subItem in item.ValidationErrors)
+                    {
+                        var message =
+                            $"Error '{subItem.ErrorMessage}' occurred in {entityTypeName} at {subItem.PropertyName}";
+                        _logger.Error(message);
+                    }
+                    
+                    // Roll back
+                    switch (entry.State)
+                    {
+                        case EntityState.Added:
+                            entry.State = EntityState.Detached;
+                            break;
+                        case EntityState.Modified:
+                            entry.CurrentValues.SetValues(entry.OriginalValues);
+                            entry.State = EntityState.Unchanged;
+                            break;
+                        case EntityState.Deleted:
+                            entry.State = EntityState.Unchanged;
+                            break;
+                    }
+                }
+                
+            }
         }
     }
 }
